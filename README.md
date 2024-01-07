2d3b2b9a6d3514600afcee2b868bbbff47d0cff3
Zhihang Yuan <hahnyuan@gmail.com>
2024-01-08 00:45:37 +0800

    support arguments

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # LLMViewer
 
 Explore the intricate facets of Large Language Models (LLMs) with LLMViewer, a powerful tool designed to provide comprehensive insights into key aspects of model inference. Uncover details such as computational load, data volume, transmission metrics, and hardware roofline model analysis. Whether you're delving into the complexities of computation, assessing various models, evaluating data transmission, or examining the theoretical hardware efficiency with the roofline model, LLMViewer offers a user-friendly interface for an in-depth exploration of these critical components in large language model inference. Elevate your understanding of LLMs with this tool.
+
+# example
+
+```bash
+python3 model_viewer.py facebook/opt-125m configs/opt.py
+python3 model_viewer.py meta-llama/Llama-2-7b-hf configs/llama-2.py
+
+```
--- model_viewer.py
+++ model_viewer.py
@@ -9,9 +9,15 @@ import torch
 import torch.nn as nn
 import numpy as np
 import os
+import importlib
+import argparse
 
-model_id = "meta-llama/Llama-2-7b-hf"
-model_id = "facebook/opt-125m"
+parser = argparse.ArgumentParser()
+parser.add_argument("model_id", type=str, help="model id")
+parser.add_argument("config_path", type=str, help="config file")
+args = parser.parse_args()
+
+model_id = args.model_id
 batchsize = 1
 seqlen = 1024
 
@@ -33,21 +39,25 @@ for name, module in model.named_modules():
         else:
             prefill_layer_mac[name] = module.weight.numel() * batchsize
 
-# breakpoint()
-from configs.opt import *
+# from configs.opt import *
+# import using import lib
+config_module = importlib.import_module(
+    args.config_path.replace("/", ".").replace(".py", "")
+)
+
 
 # num_attention_heads_config = "num_attention_heads"
 # num_attention_heads = config.getattr(num_attention_heads_config)
-num_attention_heads = getattr(config, num_attention_heads_config)
+num_attention_heads = getattr(config, config_module.num_attention_heads_config)
 # hidden_size_config = "hidden_size"
 # hidden_size = config.getattr(hidden_size_config)
-hidden_size = getattr(config, hidden_size_config)
+hidden_size = getattr(config, config_module.hidden_size_config)
 # num_key_value_heads_config = "num_key_value_heads"
 # num_key_value_heads = config.getattr(num_key_value_heads_config)
-num_key_value_heads = getattr(config, num_key_value_heads_config)
+num_key_value_heads = getattr(config, config_module.num_key_value_heads_config)
 # num_hidden_layers_config = "num_hidden_layers"
 # num_hidden_layers = config.getattr(num_hidden_layers_config)
-num_hidden_layers = getattr(config, num_hidden_layers_config)
+num_hidden_layers = getattr(config, config_module.num_hidden_layers_config)
 
 for layeri in range(num_hidden_layers):
     qk_name = f"layer{layeri}.qk"
@@ -74,10 +84,18 @@ save_path = f"output/{model_id[:model_id.rfind('/')]}"
 if not os.path.exists(save_path):
     os.makedirs(save_path)
 
-# mac
+# decode mac
 mac_file = open(f"{save_path}_mac.csv", "w")
 mac_file.write("layer,mac\n")
 for k, v in decode_layer_mac.items():
+    mac_file.write(f"{k},{v}\n")
+mac_file.write(f"total,{total_mac}")
+mac_file.close()
+
+# prefill mac
+mac_file = open(f"{save_path}_mac_prefill.csv", "w")
+mac_file.write("layer,mac\n")
+for k, v in prefill_layer_mac.items():
     mac_file.write(f"{k},{v}\n")
 mac_file.write(f"total,{total_mac}")
 mac_file.close()
