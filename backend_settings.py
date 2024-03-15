cc8159517a8b14962d825fca61d68a1aa662f005
Zhihang Yuan <hahnyuan@gmail.com>
2024-03-15 19:00:13 +0800

    support opt model

--- backend_settings.py
+++ backend_settings.py
@@ -1,10 +1,16 @@
 from hardwares.hardware_params import hardware_params
 
-avaliable_model_ids=[
+avaliable_model_ids = [
     "meta-llama/Llama-2-7b-hf",
     "meta-llama/Llama-2-13b-hf",
     "meta-llama/Llama-2-70b-hf",
     "THUDM/chatglm3-6b",
+    "facebook/opt-125m",
+    "facebook/opt-1.3b",
+    "facebook/opt-2.7b",
+    "facebook/opt-6.7b",
+    "facebook/opt-30b",
+    "facebook/opt-66b",
 ]
 
-avaliable_hardwares=[_ for _ in hardware_params.keys()]
+avaliable_hardwares = [_ for _ in hardware_params.keys()]
--- configs/opt.py
+++ configs/opt.py
@@ -1,33 +1,75 @@
 def get_num_attention_heads(model_params):
     return getattr(model_params, "num_attention_heads")
 
+
 def get_hidden_size(model_params):
     return getattr(model_params, "hidden_size")
+
 
 def get_num_key_value_heads(model_params):
     return getattr(model_params, "num_attention_heads")
 
+
 def get_num_hidden_layers(model_params):
     return getattr(model_params, "num_hidden_layers")
 
+
 def get_intermediate_size(model_params):
     return getattr(model_params, "ffn_dim")
+
 
 def get_vocab_size(model_params):
     return getattr(model_params, "vocab_size")
 
 
 def get_linear_layers(model_params):
-    hidden_size=get_hidden_size(model_params)
-    intermediate_size=get_intermediate_size(model_params)
-    key_value_heads=get_num_key_value_heads(model_params)
-    attention_heads=get_num_attention_heads(model_params)
+    hidden_size = get_hidden_size(model_params)
+    intermediate_size = get_intermediate_size(model_params)
+    key_value_heads = get_num_key_value_heads(model_params)
+    attention_heads = get_num_attention_heads(model_params)
     return {
-        "q_proj":[hidden_size, hidden_size],
-        "k_proj":[hidden_size, hidden_size*key_value_heads/attention_heads],
-        "v_proj":[hidden_size, hidden_size*key_value_heads/attention_heads],
-        "out_proj":[hidden_size, hidden_size],
-        "gate_proj":[hidden_size, intermediate_size],
-        "up_proj":[hidden_size,intermediate_size],
-        "down_proj":[intermediate_size, hidden_size],
+        "q_proj": [hidden_size, hidden_size],
+        "k_proj": [hidden_size, hidden_size * key_value_heads / attention_heads],
+        "v_proj": [hidden_size, hidden_size * key_value_heads / attention_heads],
+        "out_proj": [hidden_size, hidden_size],
+        "gate_proj": [hidden_size, intermediate_size],
+        "up_proj": [hidden_size, intermediate_size],
+        "down_proj": [intermediate_size, hidden_size],
     }
+
+
+transformer_layer_graph = {
+    "input": [],
+    "attn_norm": ["input"],
+    "q_proj": ["attn_norm"],
+    "k_proj": ["attn_norm"],
+    "v_proj": ["attn_norm"],
+    "qk_matmul": ["q_proj", "k_proj"],
+    "softmax": ["qk_matmul"],
+    "sv_matmul": ["softmax", "v_proj"],
+    "out_proj": ["sv_matmul"],
+    "attn_add": ["input", "out_proj"],
+    "mlp_norm": ["attn_add"],
+    "up_proj": ["mlp_norm"],
+    "mlp_act": ["up_proj"],
+    "down_proj": ["mlp_act"],
+    "mlp_add": ["attn_add", "down_proj"],
+    "output": ["mlp_add"],
+}
+
+flashattention_transformer_layer_graph = {
+    "input": [],
+    "attn_norm": ["input"],
+    "q_proj": ["attn_norm"],
+    "k_proj": ["attn_norm"],
+    "v_proj": ["attn_norm"],
+    "fused_attention": ["q_proj", "k_proj", "v_proj"],
+    "out_proj": ["fused_attention"],
+    "attn_add": ["input", "out_proj"],
+    "mlp_norm": ["attn_add"],
+    "up_proj": ["mlp_norm"],
+    "mlp_act": ["up_proj"],
+    "down_proj": ["mlp_act"],
+    "mlp_add": ["attn_add", "down_proj"],
+    "output": ["mlp_add"],
+}
